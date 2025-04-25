e8bd457eca0528825d3b98bd5f0b99f7d7dfe408
13reathcode <new13reath@gmail.com>
2025-04-25 13:07:05 +0300

    navigation+styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="homepage" element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
